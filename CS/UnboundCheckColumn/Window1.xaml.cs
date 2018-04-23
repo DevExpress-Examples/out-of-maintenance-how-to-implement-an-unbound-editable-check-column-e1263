ed30078aaae5d55bb55c02cd359ccf2ad36b316b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/UnboundCheckColumn/Window1.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Wpf.Grid;
+
+namespace UnboundCheckColumn {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        List<TestData> list;
+        Dictionary<Guid, bool> selectedValues = new Dictionary<Guid, bool>();
+
+        public Window1() {
+            InitializeComponent();
+            list = new List<TestData>();
+            for(int i = 0; i < 20; i++) {
+                list.Add(new TestData() { Id = Guid.NewGuid(), Number = i });
+            }
+            grid.DataSource = list;
+        }
+
+        private void grid_CustomUnboundColumnData(object sender, GridColumnDataEventArgs e) {
+            if(e.Column.FieldName == "Selected") {
+                Guid key = (Guid)e.GetListSourceFieldValue("Id");
+                if(e.IsGetData) {
+                    e.Value = GetIsSelected(key);
+                }
+                if(e.IsSetData) {
+                    SetIsSelected(key, (bool)e.Value);
+                }
+            }
+        }
+        bool GetIsSelected(Guid key) {
+            bool isSelected;
+            if(selectedValues.TryGetValue(key, out isSelected))
+                return isSelected;
+            return false;
+        }
+        void SetIsSelected(Guid key, bool value) {
+            if(value)
+                selectedValues[key] = value;
+            else
+                selectedValues.Remove(key);
+        }
+
+        private void BtnInvert_Click(object sender, RoutedEventArgs e) {
+            for(int i = 0; i < list.Count; i++) {
+                bool newIsSelected = !GetIsSelected(list[i].Id);
+                int rowHandle = grid.GetRowHandleByListIndex(i);
+                grid.SetCellValue(rowHandle, "Selected", newIsSelected);
+            }
+        }
+        private void BtnGetSelected_Click(object sender, RoutedEventArgs e) {
+            string selectedIds = string.Empty;
+            foreach(Guid key in selectedValues.Keys) {
+                selectedIds += string.Format("{0}\n", key);
+            }
+            string caption = string.Format("Selected rows (Total: {0})", selectedValues.Count);
+            MessageBox.Show(selectedIds, caption);
+        }
+    }
+    public class TestData {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ VB/UnboundCheckColumn/Window1.xaml.vb
@@ -0,0 +1,97 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Wpf.Grid
+
+Namespace UnboundCheckColumn
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Private list As List(Of TestData)
+		Private selectedValues As New Dictionary(Of Guid, Boolean)()
+
+		Public Sub New()
+			InitializeComponent()
+			list = New List(Of TestData)()
+			For i As Integer = 0 To 19
+				list.Add(New TestData() With {.Id = Guid.NewGuid(), .Number = i})
+			Next i
+			grid.DataSource = list
+		End Sub
+
+		Private Sub grid_CustomUnboundColumnData(ByVal sender As Object, ByVal e As GridColumnDataEventArgs)
+			If e.Column.FieldName = "Selected" Then
+				Dim key As Guid = CType(e.GetListSourceFieldValue("Id"), Guid)
+				If e.IsGetData Then
+					e.Value = GetIsSelected(key)
+				End If
+				If e.IsSetData Then
+					SetIsSelected(key, CBool(e.Value))
+				End If
+			End If
+		End Sub
+		Private Function GetIsSelected(ByVal key As Guid) As Boolean
+			Dim isSelected As Boolean
+			If selectedValues.TryGetValue(key, isSelected) Then
+				Return isSelected
+			End If
+			Return False
+		End Function
+		Private Sub SetIsSelected(ByVal key As Guid, ByVal value As Boolean)
+			If value Then
+				selectedValues(key) = value
+			Else
+				selectedValues.Remove(key)
+			End If
+		End Sub
+
+		Private Sub BtnInvert_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			For i As Integer = 0 To list.Count - 1
+				Dim newIsSelected As Boolean = Not GetIsSelected(list(i).Id)
+				Dim rowHandle As Integer = grid.GetRowHandleByListIndex(i)
+				grid.SetCellValue(rowHandle, "Selected", newIsSelected)
+			Next i
+		End Sub
+		Private Sub BtnGetSelected_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim selectedIds As String = String.Empty
+			For Each key As Guid In selectedValues.Keys
+				selectedIds &= String.Format("{0}" & Constants.vbLf, key)
+			Next key
+			Dim caption As String = String.Format("Selected rows (Total: {0})", selectedValues.Count)
+			MessageBox.Show(selectedIds, caption)
+		End Sub
+	End Class
+	Public Class TestData
+		Private privateId As Guid
+		Public Property Id() As Guid
+			Get
+				Return privateId
+			End Get
+			Set(ByVal value As Guid)
+				privateId = value
+			End Set
+		End Property
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+	End Class
+End Namespace
